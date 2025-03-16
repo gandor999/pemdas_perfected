b6a4878b5231a9189002d8c592cd1c8becde5af1
Geodor Ruales <gandor999@gmail.com>
2025-03-17 01:08:39 +0800

    progress: finishing touches

--- Solution.java
+++ Solution.java
@@ -8,9 +8,11 @@ class Solution {
 
         // System.out.println("calculate: " + solution.calculate("(1 + 2) * (5 + (5 + 10)) / 5"));
 
-        System.out.println("calculate: " + solution.calculate("((1 + 2) * (5 + (5 + 10)) / 5)"));
+        // System.out.println("calculate: " + solution.calculate("((1 + 2) * (5 + (5 + 10)) / 5)"));
 
-        // System.out.println("calculate: " + solution.calculate("5 + 10"));
+        System.out.println("calculate: " + solution.calculate("(4 - (1 + 2) * (5 + (5 + 10)) / 5)"));
+
+        // System.out.println("calculate: " + solution.calculate("5 - 10"));
     }
 
     public int calculate(String expression) {
@@ -20,11 +22,13 @@ class Solution {
 
         String leftNumberStr = "";
 
+        // for the parenthesis
         if (strBuilder.charAt(0) == '(') {
             int count = 0;
             int startOfIndexPar = 0;
             int endIndexOfPar = 0;
 
+            // find the matching parenthesises parenthesi? parenthesisus?
             for (int i = 0; i < strBuilder.length(); i++) {
                 if (strBuilder.charAt(i) == '(') {
                     if (count == 0) {
@@ -38,6 +42,8 @@ class Solution {
 
                     if (count == 0) {
                         endIndexOfPar = i;
+                        // replace the string with the calculated version of the parenthesis already and move on
+                        // set i to scan again for this parenthises
                         strBuilder.replace(startOfIndexPar, endIndexOfPar + 1, String.valueOf(calculate(strBuilder.substring(startOfIndexPar + 1, endIndexOfPar))));
                         i = 0;
                     }
@@ -45,15 +51,20 @@ class Solution {
             }
         }
 
-        // System.out.println("string: " + strBuilder.toString() + " | length: " + strBuilder.length());
-
+        // parse the digits only
         while (Character.isDigit(strBuilder.charAt(0))) {
             leftNumberStr += String.valueOf(strBuilder.charAt(0));
             strBuilder.deleteCharAt(0);
 
+            // if no symbol is encountered then it means that this is the very last number
             if (strBuilder.isEmpty()) {
                 return Integer.valueOf(leftNumberStr);
             }
+        }
+
+        // let's say that the very last number becomes -8, then just let the left be just 0
+        if (leftNumberStr.isEmpty()) {
+            leftNumberStr = "0";
         }
 
         int leftNumber = Integer.valueOf(leftNumberStr);
